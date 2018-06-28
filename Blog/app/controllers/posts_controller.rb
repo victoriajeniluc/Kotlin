b6f29dc0962e277e298aea5f9861360064052ceb
Victoria Luc <victoriajeniluc@gmail.com>
2018-06-28 13:47:26 -0500

    add validations to the posts

--- Blog/app/controllers/posts_controller.rb
+++ Blog/app/controllers/posts_controller.rb
@@ -4,6 +4,13 @@ class PostsController < ApplicationController
 	end 
 
 	def new 
+		@post = Post.new 
+
+		if @post.save 
+			redirect_to @post 
+		else 
+			render 'new'
+		end 
 	end
 
 	def create 
