0ab65bff9acf1f126a5772cad73dcf77724a1e20
Victoria Luc <victoriajeniluc@gmail.com>
2018-07-02 18:07:57 -0500

    Style the Login page with a asset pipeline

--- Blog/app/controllers/posts_controller.rb
+++ Blog/app/controllers/posts_controller.rb
@@ -1,47 +1,55 @@
-class PostsController < ApplicationController
-	def index
-		@posts = Post.all.order('created_at DESC')
+class PostsController < ApplicationController 
+
+	before_action :authorize 
+	
+	def index 
+		@posts = Post.all.order('created_at DESC') 
 	end 
 
 	def new 
 		@post = Post.new 
-	end
+	end 
 
+	# to save our post... we have make a post 
 	def create 
 		@post = Post.new(post_params)
+		@post.user_id = current_user.id 
 		if @post.save 
-			redirect_to @post 
+			redirect_to @post
 		else 
+			@errors = @post.errors.full_messages 
 			render 'new'
-		end 
-	end  
+		end 	
+	end 
 
-	def show
+	def show 
 		@post = Post.find(params[:id])
 	end 
 
-	def edit 
+	def edit
 		@post = Post.find(params[:id])
 	end 
 
 	def update 
 		@post = Post.find(params[:id])
 
-		if @post.update(params[:post].permit(:title, :body, :gratitude))
+		if @post.update(params[:post].permit(:title, :body))
 			redirect_to @post
 		else 
+			@errors = @post.errors.full_messages
 			render 'edit'
 		end 
 	end 
-
-	def destroy 
+	
+	def destroy
 		@post = Post.find(params[:id])
-		@post.destroy 
+		@post.destroy
 		redirect_to posts_path
-	end
-
-	private 
-	def post_params 
-		params.require(:post).permit(:title, :body, :gratitude)
 	end 
-end
+
+	# rails 4 has a strong parameter where you have explicity state what you want to allow.. so you have to make a private method 
+	private 
+		def post_params 
+			params.require(:post).permit(:title, :body)
+		end 
+end 
